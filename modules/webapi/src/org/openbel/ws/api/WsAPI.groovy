29e2da3e6dc949bb0e01e6fd19a4ae6d5aaa3863
Anthony Bargnesi <abargnesi@selventa.com>
2013-08-13 08:49:44 -0400

    non-blocking load of kn with callback closure
    added more api documentation

--- modules/webapi/src/org/openbel/ws/api/WsAPI.groovy
+++ modules/webapi/src/org/openbel/ws/api/WsAPI.groovy
@@ -5,7 +5,38 @@ package org.openbel.ws.api
  */
 interface WsAPI {
 
-    Map loadKnowledgeNetwork(name)
+    /**
+     * Load a knowledge network using the unique name.
+     * <br/>
+     * This method is non-blocking.  When the knowledge network load finishes the
+     * {@code callback} closure is called, with the load data {@link Map}, if
+     * provided.
+     * <br/>
+     * The map will contain:
+     * <ul>
+     *     <li><strong>status</strong></li>
+     *     <ul>
+     *         <li>FAILED or COMPLETE</li>
+     *         <li>An IN_PROCESS status prompts subsequent requests at a small rate
+     *         thus it will never be returned.</li>
+     *     </ul>
+     *     <li><strong>message</strong></li>
+     *     <ul>
+     *         <li>The loaded message that reflects the <strong>status</strong>.</li>
+     *     </ul>
+     *     <li><strong>handle</strong> (optional)</li>
+     *     <ul>
+     *         <li>A string token that represents a handle to a knowledge network.</li>
+     *         <li>The <strong>handle</strong> will only be present when the returned
+     *         <strong>status</strong> is <strong>COMPLETE</strong>.</li>
+     *     </ul>
+     * </ul>
+     *
+     * @param name {@link String} knowledge network name; may not be {@code null}
+     * @param callback {@link Closure} optional callback called when the load finishes
+     * @throws NullPointerException when {@code name} is {@code null}
+     */
+    void loadKnowledgeNetwork(String name, Closure callback)
 
     /**
      * Returns the provided knowledge networks as a {@link Map} where the
--- modules/webapi/src/org/openbel/ws/internal/BasicWsAPI.groovy
+++ modules/webapi/src/org/openbel/ws/internal/BasicWsAPI.groovy
@@ -3,51 +3,64 @@ package org.openbel.ws.internal
 import org.openbel.ws.api.WsAPI
 import wslite.soap.*
 
+/**
+ * {@link WsAPI} implementation using groovy-wslite (soap xml builders).
+ */
 class BasicWsAPI implements WsAPI {
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    Map loadKnowledgeNetwork(name) {
+    void loadKnowledgeNetwork(String name, Closure closure = null) {
         def client = new SOAPClient('http://demo.openbel.org/openbel-ws/belframework')
         def loadMap = [name: name]
-        def response = client.send {
-            body {
-                LoadKamRequest('xmlns': 'http://belframework.org/ws/schemas') {
-                    kam {
-                        mkp.yieldUnescaped "<name>${name}</name>"
-                    }
-                }
-            }
-        }
-        def resMap = response.LoadKamResponse[0].flatMap()
-        loadMap.status = resMap.LoadKamResponse._children.find { it.loadStatus }.loadStatus._text
-        while (loadMap.status == 'IN_PROCESS') {
-            new Timer().runAfter(1000, {
-                response = client.send {
-                    body {
-                        LoadKamRequest('xmlns': 'http://belframework.org/ws/schemas') {
-                            kam {
-                                mkp.yieldUnescaped "<name>${name}</name>"
-                            }
+
+        Thread.start {
+            def response = client.send {
+                body {
+                    LoadKamRequest('xmlns': 'http://belframework.org/ws/schemas') {
+                        kam {
+                            mkp.yieldUnescaped "<name>${name}</name>"
                         }
                     }
                 }
-                resMap = response.LoadKamResponse[0].flatMap()
-                loadMap.status = resMap.LoadKamResponse._children.find { it.loadStatus }.loadStatus._text
-            })
-            sleep(1000)
-        }
+            }
+            def resMap = response.LoadKamResponse[0].flatMap()
+            loadMap.status = resMap.LoadKamResponse._children.find { it.loadStatus }.loadStatus._text
+            while (loadMap.status == 'IN_PROCESS') {
+                new Timer().runAfter(1000, {
+                    response = client.send {
+                        body {
+                            LoadKamRequest('xmlns': 'http://belframework.org/ws/schemas') {
+                                kam {
+                                    mkp.yieldUnescaped "<name>${name}</name>"
+                                }
+                            }
+                        }
+                    }
+                    resMap = response.LoadKamResponse[0].flatMap()
+                    loadMap.status = resMap.LoadKamResponse._children.find { it.loadStatus }.loadStatus._text
+                })
+                sleep(1000)
+            }
 
-        if (loadMap.status == 'FAILED') {
-            loadMap.message = resMap.LoadKamResponse._children.find { it.message }.message._text
-        } else if (loadMap.status == 'COMPLETE') {
-            loadMap.handle = resMap.LoadKamResponse._children.find { it.handle }.handle._children[0].handle._text
-            loadMap.message = "${name} was loaded successfully."
-        } else if (loadMap.status == 'IN_PROCESS') {
-            loadMap.message = "${name} is in the process of loading."
+            if (loadMap.status == 'FAILED') {
+                loadMap.message = resMap.LoadKamResponse._children.find { it.message }.message._text
+            } else if (loadMap.status == 'COMPLETE') {
+                loadMap.handle = resMap.LoadKamResponse._children.find { it.handle }.handle._children[0].handle._text
+                loadMap.message = "${name} was loaded successfully."
+            } else if (loadMap.status == 'IN_PROCESS') {
+                loadMap.message = "${name} is in the process of loading."
+            }
+
+            if (closure) closure.call(loadMap)
         }
-        loadMap
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override Map knowledgeNetworks() {
         def client = new SOAPClient('http://demo.openbel.org/openbel-ws/belframework')
         def response = client.send {
@@ -62,16 +75,25 @@ class BasicWsAPI implements WsAPI {
             collectEntries { k, v -> [(k): v.first()]}
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     Map[] adjacentEdges(node) {
         return new Map[0]
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     Map[] resolveNodes(name, nodes) {
         return new Map[0]
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     Map[] resolveEdges(name, edges) {
         return new Map[0]
@@ -86,11 +108,11 @@ class BasicWsAPI implements WsAPI {
         println()
 
         if (kams) {
-            def name = kams.take(1).values().first()._children.find({it.name}).name._text
+            String name = kams.take(1).values().first()._children.find({it.name}).name._text
             println "LoadKam...Grab first one ($name)"
-            println api.loadKnowledgeNetwork(name)
+            api.loadKnowledgeNetwork(name) { println it }
         }
         println "LoadKam...Does not exist"
-        println api.loadKnowledgeNetwork('Does not exist')
+        api.loadKnowledgeNetwork('Does not exist') { println it }
     }
 }
