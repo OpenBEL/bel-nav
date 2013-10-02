908225bfec2fc6972a0b0c8e197da40b5444f971
Anthony Bargnesi <abargnesi@selventa.com>
2013-10-01 22:09:41 -0400

    removed unused 'main' module

--- modules/main/src/org/openbel/kamnav/main/Activator.groovy
+++
@@ -1,9 +0,0 @@
-package org.openbel.kamnav.main
-
-import org.cytoscape.service.util.AbstractCyActivator
-import org.osgi.framework.BundleContext
-
-class Activator extends AbstractCyActivator {
-
-    void start(BundleContext bc) {}
-}
