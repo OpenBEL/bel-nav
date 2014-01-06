60d00344effe397ed4791ae27fcfae54898415b8
Anthony Bargnesi <abargnesi@selventa.com>
2014-01-06 12:41:30 -0500

    update bel.function when name changes

--- modules/core/src/org/openbel/kamnav/core/event/BELNetworkListener.groovy
+++ modules/core/src/org/openbel/kamnav/core/event/BELNetworkListener.groovy
@@ -1,15 +1,22 @@
 package org.openbel.kamnav.core.event
 
 import groovy.transform.TupleConstructor
-import org.cytoscape.model.events.AddedNodesEvent
-import org.cytoscape.model.events.AddedNodesListener
+import org.cytoscape.model.CyRow
 import org.cytoscape.model.events.NetworkAddedEvent
 import org.cytoscape.model.events.NetworkAddedListener
+import org.cytoscape.model.events.RowSetRecord
+import org.cytoscape.model.events.RowsSetEvent
+import org.cytoscape.model.events.RowsSetListener
 import org.cytoscape.work.TaskIterator
+import org.openbel.framework.common.InvalidArgument
+import org.openbel.framework.common.model.Term
 import org.openbel.kamnav.core.task.AddBelColumnsToCurrent
+import static org.cytoscape.model.CyNetwork.NAME
+import static org.openbel.framework.common.bel.parser.BELParser.parseTerm
+import static org.openbel.kamnav.common.util.Util.createColumn
 
 @TupleConstructor
-class BELNetworkListener implements NetworkAddedListener, AddedNodesListener {
+class BELNetworkListener implements NetworkAddedListener, RowsSetListener {
 
     final Expando cyr
 
@@ -25,6 +32,23 @@ class BELNetworkListener implements NetworkAddedListener, AddedNodesListener {
     }
 
     @Override
-    void handleEvent(AddedNodesEvent evt) {
+    void handleEvent(RowsSetEvent evt) {
+        if (!evt.containsColumn(NAME)) return
+        evt.getColumnRecords(NAME).each(this.&addFunction)
+    }
+
+    private static void addFunction(RowSetRecord record) {
+        createColumn(record.row.table, 'bel.function', String.class, false, null)
+        def name = record.row.get(NAME, String.class)
+        if (name) {
+            try {
+                Term t = parseTerm(name)
+                if (t) {
+                    record.row.set('bel.function', t.functionEnum.displayValue)
+                }
+            } catch (InvalidArgument e) {
+                // indicates failure to parse; skip
+            }
+        }
     }
 }
